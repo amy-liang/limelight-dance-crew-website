7980ffc72be1d7f4a4f45a5b3b4c2cd41c342536
amy-liang <amy.liaxx@gmail.com>
2020-05-23 14:43:32 -0400

    Init project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import {Component} from "react";
+import * as React from "react";
+
+export class Home extends Component<{},{}>{
+    render() {
+        return <div>LIMELIGHT DANCE CREW</div>
+    }
+}
